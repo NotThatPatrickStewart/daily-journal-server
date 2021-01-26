0985b0aad677c1d9938d704efe8cb97c38d81e23
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-01-26 15:48:34 -0600

    added do_GET function for entries

--- request_handler.py
+++ request_handler.py
@@ -1,4 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+# import json
+from entries import get_all_entries
+from entries import get_single_entry
 
 
 # Here's a class. It inherits from another class.
@@ -6,6 +9,42 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 # work together for a common purpose. In this case, that
 # common purpose is to respond to HTTP requests from a client.
 class HandleRequests(BaseHTTPRequestHandler):
+
+    def parse_url(self, path): #need self when function is inside a class
+        # Just like splitting a string in JavaScript. If the
+        # path is "/animals/2", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
+        # at index 2.
+        path_params = path.split("/") #split on /, create list: ['', 'animals', '2'] where 2 is the animal id
+        resource = path_params[1] #because of the empty string at index [0]
+        
+        # Check if there is a query string parameter
+        if "?" in resource:
+            # GIVEN: /customers?email=jenna@solis.com
+
+            param = resource.split("?")[1]  # email=jenna@solis.com
+            resource = resource.split("?")[0]  # 'customers'
+            pair = param.split("=")  # [ 'email', 'jenna@solis.com' ]
+            key = pair[0]  # 'email'
+            value = pair[1]  # 'jenna@solis.com'
+
+            return ( resource, key, value )
+        
+        # No query string parameter
+        else:
+            id = None
+
+            # Try to get the item at index 2
+            try:
+            # Convert the string "1" to the integer 1
+                # This is the new parseInt()
+                id = int(path_params[2])
+            except IndexError:
+                pass  # No route parameter exists: /animals
+            except ValueError:
+                pass  # Request had trailing slash: /animals/
+
+            return (resource, id)  # This is a tuple
 
     # Here's a class function
     def _set_headers(self, status):
@@ -21,6 +60,28 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
         self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
         self.end_headers()
+
+    def do_GET(self):
+        self._set_headers(200)
+
+        response = {}
+
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
+
+        # Response from parse_url() is a tuple with 2
+        # items in it, which means the request was for
+        # `/enties` or `/enties/2`
+        if len(parsed) == 2:
+            ( resource, id ) = parsed
+
+            if resource == "entries":
+                if id is not None:
+                    response = f"{get_single_entry(id)}"
+                else:
+                    response = f"{get_all_entries()}"
+
+        self.wfile.write(response.encode())
 
 # This function is not inside the class. It is the starting
 # point of this application.
